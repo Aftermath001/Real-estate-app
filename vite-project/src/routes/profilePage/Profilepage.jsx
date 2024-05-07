67ef18d1a3761c135ac167196fd37c67d4e2f174
Aftermath001 <alvinasiachi@gmail.com>
2024-05-07 10:07:46 +0300

    Profile page

--- vite-project/src/routes/profilePage/Profilepage.jsx
+++ vite-project/src/routes/profilePage/Profilepage.jsx
@@ -2,7 +2,37 @@ import React from 'react'
 import './profilepage.scss'
 function Profilepage() {
   return (
-    <div>Profilepage</div>
+    <div classNAme='profilepage'>
+      <div className="details">
+        <div className="wrapper">
+          <div className="title">
+            <h1>User Information</h1>
+            <button>Update Profile</button>
+          </div>
+
+          <div className="info">
+            <span>
+              Avatar:{""}
+              <img src='https://images.unsplash.com/photo-1511367461989-f85a21fda167?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZmlsZXxlbnwwfHwwfHx8MA%3D%3D' alt='userprofile'/>
+              <span> Username: <b>John Doe</b></span>
+              <span> Email: <b>john@gmail.com</b></span>
+            </span>
+          </div>
+
+          <div className="title">
+            <h1>My List</h1>
+            <button>Create New Post</button>
+          </div>
+
+          <div className="title">
+            <h1>Saved List</h1>
+          </div>
+        </div>
+      </div>
+      <div className="chatContainer">
+        <div className="wrapper"></div>
+      </div>
+    </div>
   )
 }
 
